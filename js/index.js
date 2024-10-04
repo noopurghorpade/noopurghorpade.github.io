ded2fd905f768b799adde013ccba4bcaede021a5
noopurghorpade <noopur.g7@gmail.com>
2024-10-05 00:35:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -86,79 +86,82 @@ onload = function () {
     // textLoad();
     // setInterval(textLoad, 6000);
 
+    // Word functionality
     var words = document.getElementsByClassName('word');
     var wordArray = [];
     var currentWord = 0;
 
-    words[currentWord].style.opacity = 1;
-    for (var i = 0; i < words.length; i++) {
-        splitLetters(words[i]);
-    }
-
-    function changeWord() {
-        var cw = wordArray[currentWord];
-        var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-        for (var i = 0; i < cw.length; i++) {
-            animateLetterOut(cw, i);
+    // Ensure words exist
+    if (words.length > 0) {
+        words[currentWord].style.opacity = 1;
+        for (var i = 0; i < words.length; i++) {
+            splitLetters(words[i]);
         }
 
-        for (var i = 0; i < nw.length; i++) {
-            nw[i].className = 'letter behind';
-            nw[0].parentElement.style.opacity = 1;
-            animateLetterIn(nw, i);
-        }
-
-        currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
-    }
-
-    function animateLetterOut(cw, i) {
-        setTimeout(function () {
-            cw[i].className = 'letter out';
-        }, i * 20);
-    }
-
-    function animateLetterIn(nw, i) {
-        setTimeout(function () {
-            nw[i].className = 'letter in';
-        }, 340 + (i * 20));
-    }
-
-    function splitLetters(word) {
-        var content = word.innerHTML;
-        word.innerHTML = '';
-        var letters = [];
-        for (var i = 0; i < content.length; i++) {
-            var letter = document.createElement('span');
-            letter.className = 'letter';
-            letter.innerHTML = content.charAt(i);
-            word.appendChild(letter);
-            letters.push(letter);
-        }
-
-        wordArray.push(letters);
-    }
-
-    changeWord();
-    setInterval(changeWord, 2000);
-
-    function splitLetters(word) {
-        var content = word.textContent;  // Use textContent instead of innerHTML
-        word.innerHTML = '';  // Clear the content
-        var letters = [];
-        for (var i = 0; i < content.length; i++) {
-            var letter = document.createElement('span');
-            letter.className = 'letter';
-            if (content.charAt(i) === ' ') {
-                letter.innerHTML = '&nbsp;';  // Add non-breaking space
-            } else {
-                letter.innerHTML = content.charAt(i);
+        function changeWord() {
+            var cw = wordArray[currentWord];
+            var nw = currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+            for (var i = 0; i < cw.length; i++) {
+                animateLetterOut(cw, i);
             }
-            word.appendChild(letter);
-            letters.push(letter);
+    
+            for (var i = 0; i < nw.length; i++) {
+                nw[i].className = 'letter behind';
+                nw[0].parentElement.style.opacity = 1;
+                animateLetterIn(nw, i);
+            }
+    
+            currentWord = (currentWord == wordArray.length - 1) ? 0 : currentWord + 1;
+        }
+    
+        function animateLetterOut(cw, i) {
+            setTimeout(function () {
+                cw[i].className = 'letter out';
+            }, i * 20);
+        }
+    
+        function animateLetterIn(nw, i) {
+            setTimeout(function () {
+                nw[i].className = 'letter in';
+            }, 340 + (i * 20));
         }
 
-        wordArray.push(letters);
+        function splitLetters(word) {
+            var content = word.innerHTML; // Get the content of the word
+            word.innerHTML = ''; // Clear the existing content
+            var letters = [];
+
+            // Use a regular expression to split content into an array, while retaining &nbsp;
+            var splitContent = content.split(/(&nbsp;| )/); // Split by spaces and &nbsp;
+
+            splitContent.forEach((char) => {
+                var letter = document.createElement('span');
+                letter.className = 'letter';
+
+                if (char === '&nbsp;') {
+                    // For &nbsp; create a visual space
+                    letter.style.display = 'inline-block'; // Make it behave like a letter
+                    letter.style.width = '10px'; // Adjust width for spacing (as needed)
+                } else if (char === ' ') {
+                    // For regular spaces, also create a visual space
+                    letter.style.display = 'inline-block';
+                    letter.style.width = '10px'; // Adjust width for spacing (as needed)
+                } else {
+                    letter.innerHTML = char; // Regular character
+                }
+
+                word.appendChild(letter); // Append the letter span to the word
+                letters.push(letter); // Store the letter span in the letters array
+            });
+
+            wordArray.push(letters); // Store the letters array for future animations
+        }
+
+
+        changeWord();
+        setInterval(changeWord, 2000);
     }
+
     setTimeout(function () { // allowing 3 secs to fade out loader
         $('.page-loader').fadeOut('slow');
     }, 3500);
@@ -176,6 +179,8 @@ onload = function () {
     circleBtn.addEventListener('mouseleave', () => {
         circleBtn.style.transform = 'translate(0, 0)';
     });
+
+
 
 }
 
